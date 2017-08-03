92b0764b531bd6217793d1915ccca12b0af09b5f
Dave Pirotte <dave@braintreepayments.com>
2017-08-03 12:43:36 -0500

    * Lock owners now delete their locks atomically. * Consistently use px instead of ex for expiration. * Gitignore redis database dump.

--- lib/redis_locking_cache.rb
+++ lib/redis_locking_cache.rb
@@ -15,6 +15,7 @@ class RedisLockingCache
     expires_in = opts.fetch(:expires_in, 1)
     expires_in_ms = (expires_in * 1000).to_i
     lock_timeout = opts.fetch(:lock_timeout, 1)
+    lock_timeout_ms = (lock_timeout * 1000).to_i
     lock_wait = opts.fetch(:lock_wait, 0.025)
     cache_wait = opts.fetch(:cache_wait, 1)
 
@@ -29,15 +30,13 @@ class RedisLockingCache
       while cached.nil? && Time.now.to_f < cache_wait_expiry
         unless cached = @redis.get(key)
           lock_id = SecureRandom.hex(16)
-          if @redis.set(lock_key, lock_id, nx: true, ex: lock_timeout)
+          if @redis.set(lock_key, lock_id, nx: true, px: lock_timeout_ms)
             begin
               cached = block.call
               @redis.set(key, cached)
               @redis.set(expiry_key, 1, px: expires_in_ms)
             ensure
-              if @redis.get(lock_key) == lock_id
-                @redis.del(lock_key)
-              end
+              @redis.eval('if redis.call("get",KEYS[1]) == ARGV[1] then return redis.call("del",KEYS[1]) end', [lock_key], [lock_id])
             end
           else
             sleep(lock_wait)
@@ -45,7 +44,6 @@ class RedisLockingCache
         end
       end
     elsif expiry.nil?
-      # TODO remove lock key
       lock_id = SecureRandom.hex(16)
       if @redis.set(lock_key, 1, nx: true, ex: lock_timeout)
         begin
@@ -55,9 +53,7 @@ class RedisLockingCache
         rescue
           # TODO bubble up errors when appropriate
         ensure
-          if @redis.get(lock_key) == lock_id
-            @redis.del(lock_key)
-          end
+          @redis.eval('if redis.call("get",KEYS[1]) == ARGV[1] then return redis.call("del",KEYS[1]) end', [lock_key], [lock_id])
         end
       end
     end
